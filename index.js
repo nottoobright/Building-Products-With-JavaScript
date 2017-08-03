63bc2fc4512ba3db33bde9585e4d985ac822130a
nottoobright <puthranyash1@gmail.com>
2017-08-03 20:41:14 +0530

    Basic React router setup

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({ width: 800, height: 600 })
 
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000/inde.html'
+    mainWindow.loadURL('http://localhost:3000/'
         /* url.format({
             pathname: path.join(__dirname, 'index.html'),
             protocol: 'file:',
